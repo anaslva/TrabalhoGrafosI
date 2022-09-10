109f4abf896e6cf7d95923f2b4ea70ea3b8ec2e6
anaslva <anacarolina.slva@hotmail.com>
2022-09-09 22:29:09 -0300

    Implementação de graus e arestas

--- Grafo.java
+++ Grafo.java
@@ -1,21 +1,18 @@
 public class Grafo {
 
-    int grafo[][] = new int[3][3];
-    int qtdVertice;
+    int grafo[][] = new int[1][1];
 
     public Grafo(){
-        grafo[0][0] = 0;
-        grafo[0][1] = 1;
-        grafo[0][2] = 1;
-        grafo[1][0] = 1;
-        grafo[1][1] = 0;
-        grafo[1][2] = 1;
-        grafo[2][0] = 1;
-        grafo[2][1] = 1;
-        grafo[2][2] = 0;
 
-        System.out.println("grau: " + grausDoVertice(grafo));
-        System.out.println("tipo: " + tipoDoGrafo(grafo));
+        grafo[0][0] = 1;
+
+        System.out.println("GRAU: " + grausDoVertice(grafo));
+        System.out.println("=================================================");
+
+        System.out.println("TIPO: \n" + tipoDoGrafo(grafo));
+        System.out.println("=================================================");
+
+        System.out.println("ARESTAS: \n" + arestasDoGrafo(grafo));
 
     }
 
@@ -26,25 +23,25 @@ public class Grafo {
     private String tipoDoGrafo(int grafo[][]){
         String tipoDoGrafo = "";
         if(ehDirigido(grafo))
-            tipoDoGrafo += "dirigido, ";
+            tipoDoGrafo += "-dirigido \n";
         else
-            tipoDoGrafo += "não dirigido, ";
+            tipoDoGrafo += "-não dirigido \n";
 
         if(ehSimples(grafo))
-            tipoDoGrafo += "simples, ";
+            tipoDoGrafo += "-simples \n";
         else
-            tipoDoGrafo += "multigrafo, ";
+            tipoDoGrafo += "-multigrafo \n";
 
         if(ehNulo(grafo)){
-            tipoDoGrafo += "nulo, ";
+            tipoDoGrafo += "-nulo \n";
         }
 
         if(ehRegular(grafo)){
-            tipoDoGrafo += "regular, ";
+            tipoDoGrafo += "-regular \n";
         }
 
         if(ehCompleto(grafo)){
-            tipoDoGrafo += "completo, ";
+            tipoDoGrafo += "-completo \n";
         }
 
         //if(ehBipartido()){
@@ -57,39 +54,69 @@ public class Grafo {
 
     public String arestasDoGrafo(int grafo[][]){
         int qtdArestas = 0;
-        String conjuntoArestas = "";
+        String conjuntoArestas = "{ ";
+        Boolean ehDirigido = ehDirigido(grafo);
 
-        if(!ehDirigido(grafo)){
             for (int i= 0; i <= grafo.length - 1; i++){
                 for (int j=0; j <=grafo[i].length - 1; j++){
                     if(grafo[i][j] > 0){
-                        ehNulo = false;
-                        return ehNulo;
+                        if(!ehDirigido && !conjuntoArestas.contains("(" + j + "," + i + ") " )){
+                            conjuntoArestas += "(" + i + "," + j + ") ";
+                        } else {
+                            conjuntoArestas += "(" + i + "," + j + ") ";
+                        }
+                        if(i == j && !ehDirigido ){
+                            qtdArestas += grafo[i][j] * 2;
+                        } else {
+                            qtdArestas += grafo[i][j];
+                        }
                     }
                 }
             }
-        }
+
+        return "Quantidade de arestas: " + (!ehDirigido ? qtdArestas/2 : qtdArestas) + "\nConjunto de arestas = " + conjuntoArestas  + "}";
+
     }
 
     public String grausDoVertice(int grafo[][]){
         String grausDoVertice = "";
+        int[] elementos = getGrauDeVertices(grafo);
+        int[] sequencia = sequenciaDeGrau(elementos);
         if(!ehDirigido(grafo)){
-            int[] elementos = getGrauDeVerticesNaoDirigido(grafo);
-            int[] sequencia = sequenciaDeGrau(elementos);
             for (int i= 0; i <= elementos.length - 1; i++){
 
                grausDoVertice += "\n Vértice " + i + ": " + elementos[i];
 
             }
-
             grausDoVertice += "\n Sequência de graus: ";
             for (int i= 0; i <= sequencia.length- 1; i++){
 
                 grausDoVertice += sequencia[i] + " ";
-
             }
-        }
+        } else {
+            int[] qtdEntradas = new int[grafo.length];
+            int[] qtdSaidas = new int[grafo.length];
+            for (int i= 0; i <= grafo.length - 1; i++){
+                for (int j=0; j <=grafo[i].length - 1; j++){
+                        if(grafo[i][j] > 0){
+                            qtdEntradas[i] += grafo[i][j];
+                            qtdSaidas[j] += grafo[i][j];
+                        }
+                    }
+                }
 
+            for(int i = 0; i <= grafo.length - 1; i++){
+                grausDoVertice += "\nVértice " + i +
+                        "\n Entradas: " + qtdEntradas[i] +
+                        "\n Saídas: " + qtdSaidas[i];
+
+                }
+            }
+
+        grausDoVertice += "\n Sequência de graus: ";
+        for (int i= 0; i <= sequencia.length- 1; i++){
+            grausDoVertice += sequencia[i] + " ";
+        }
         return grausDoVertice;
     }
 
@@ -145,26 +172,29 @@ public class Grafo {
     }
 
     private Boolean ehRegular(int grafo[][]){
-
-        int[] graus = getGrauDeVerticesNaoDirigido(grafo);
-        int grau = graus[0];
-        for(int i = 0; i <= graus.length - 1; i++){
-            if(graus[i] != grau) {
-                return false;
+        int[] graus = getGrauDeVertices(grafo);
+        if(graus.length > 0){
+            int grau = graus[0];
+            for(int i = 0; i <= graus.length - 1; i++){
+                if(graus[i] != grau) {
+                    return false;
+                }
             }
         }
-
         return true;
     }
 
-    public int[] getGrauDeVerticesNaoDirigido(int grafo[][]){
+    public int[] getGrauDeVertices(int grafo[][]){
+        Boolean ehDirigido = ehDirigido(grafo);
         int[] graus = new int[grafo.length];
 
         for (int i= 0; i <= grafo.length - 1; i++){
             graus[i] = 0;
             for (int j=0; j <=grafo[i].length - 1; j++){
-                if(i == j){
+                if(i == j && !ehDirigido){
                     graus[i] = graus[i] + (grafo[i][j] * 2);
+                } else {
+                    graus[i] += grafo[i][j];
                 }
 
             }
@@ -174,17 +204,24 @@ public class Grafo {
     }
 
     private Boolean ehCompleto(int grafo[][]){
-        int valor = grafo[0][1];
-        if(grafo.length == 1){
-            return true;
-        }
-        for (int i= 0; i <= grafo.length - 1; i++){
-            for (int j=0; j <=grafo[i].length - 1; j++){
-                if(grafo[i][j] != valor && i != j){
-                    return false;
+        if(grafo.length > 1){
+            int valor = grafo[0][1];
+            if(grafo.length == 1){
+                return true;
+            }
+            for (int i= 0; i <= grafo.length - 1; i++){
+                for (int j=0; j <=grafo[i].length - 1; j++){
+                    if(grafo[i][j] != valor && i != j){
+                        return false;
+                    }
                 }
             }
+        } else {
+            if(grafo[0][0] > 0){
+                return false;
+            }
         }
+
         return true;
     }
 
@@ -202,6 +239,8 @@ public class Grafo {
 
         return elementos;
     }
+
+
 
     public static void main(String[] args) {
         Grafo g = new Grafo();
